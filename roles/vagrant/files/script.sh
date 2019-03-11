618d327cbd0c27f60fa275e802c3a2c78dd414cc
Ivan Evseichik <ivan.evseichik@gmail.com>
2019-03-11 23:09:31 +0300

    add config for copy id_rsa to slave machine

--- roles/vagrant/files/script.sh
+++ roles/vagrant/files/script.sh
@@ -5,6 +5,9 @@ sudo apt-get  install -y oracle-java8-installer git -q
 sudo useradd  -m -d /var/lib/jenkins jenkins
 sudo  -u jenkins bash -c 'mkdir /var/lib/jenkins/.ssh'
 sudo -u jenkins bash -c 'cat /tmp/id_rsa.pub >> /var/lib/jenkins/.ssh/authorized_keys'
+sudo -u jenkins bash -c 'cat /tmp/id_rsa >> /var/lib/jenkins/.ssh/id_rsa'
 chown jenkins:jenkins /var/lib/jenkins/.ssh/authorized_keys
 chmod 600 /var/lib/jenkins/.ssh/authorized_keys
-rm -f /tmp/authorized_keys
+chown jenkins:jenkins /var/lib/jenkins/.ssh/id_rsa
+chmod 600 /var/lib/jenkins/.ssh/id_rsa
+rm -f /tmp/id_r*
